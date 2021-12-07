9b9bbf94f07c35836567ab13bb14aaeab8f0b928
MuffinPlease <benjamintec3455@gmail.com>
2021-12-06 22:07:43 -0600

    Removing test variables

--- code.js
+++ code.js
@@ -1,8 +1,5 @@
 //Helpers
 const workers = [];
-const sueldo = 200;
-const minHours = 40;
-const bonus = 50;
 
 function obtainHours(list){
     const hours = list.map(
@@ -199,24 +196,3 @@ function calculateData(){
         alert("Complete the data");
 
 }
-//Example
-workers.push({
-    name: "a",
-    hours: 40,
-    sex: 1
-});
-workers.push({
-    name: "b",
-    hours: 40,
-    sex: 2
-});
-workers.push({
-    name: "c",
-    hours: 38,
-    sex: 3
-});
-workers.push({
-    name: "d",
-    hours: 42,
-    sex: 1
-});
