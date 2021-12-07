de9cd3c5e6d19c9278fd31e8a76bf478ef3d5146
MuffinPlease <benjamintec3455@gmail.com>
2021-12-06 19:58:15 -0600

    Ct main - Js working on html

--- code.js
+++ code.js
@@ -14,12 +14,38 @@ function obtainHours(list){
     return hours;
 }
 
+function writeText(list){
+    let cont = 0;
+    const text = list.map(
+        function (element){
+            cont++;
+            const paragraph = `Worker ${cont}:\n${element.name} with ${element.hours} hours worked\n`;
+
+            return paragraph
+        }
+    );
+
+    //Return
+    return text;
+}
+
+function obtainText(text,element){
+    return (text + element);
+}
+
+function restarArray(array){
+    const size = array.length;
+    for (let i = 0; i < size; i++) {
+        array.pop();   
+    }
+}
+
 const calculateAverage = x => (x.reduce((x,y) => x+y))/x.length;
 const order = (x,y) => (x.hours-y.hours);
 
 
 //Function
-function totalToPay(list,minHours,salary,bonus){
+function totalToPay(list,minHours,salary,bonus = 0){
     //Functions
     const sumHours= (acum,hour) => {
         const pay = hour*salary;
@@ -45,13 +71,6 @@ function totalToPay(list,minHours,salary,bonus){
 
     const toPayBonus = bonusHours.reduce(sumHoursBonus,0);
 
-    console.log(
-        hours,
-        bonusHours,
-        toPay,
-        toPayBonus
-    )
-
     const total = toPay + toPayBonus;
 
     //Return
@@ -64,16 +83,7 @@ function averangeHours(list){
     return calculateAverage(hours);
 }
 
-function calculateTop10(list){
-    const sortList = list.sort(order);
 
-    const spliceStart = Math.floor((sortList.length * (9)/10));
-    const spliceCount = sortList.length - spliceStart;
-
-    const listTop10 = sortList.splice(spliceStart,spliceCount);
-
-    return listTop10;
-}
 
 function hoursQuote(list,minHours){
     const notComply = list.filter(
@@ -87,6 +97,99 @@ function hoursQuote(list,minHours){
     return notComply;
 }
 
+//Html-------------------------------
+function addWorker() {
+    //Inputs
+    const inputForm = document.getElementById("workerForm");
+    const inputName = document.getElementById("name");
+    const inputHours = document.getElementById("hours");
+    const inputSex = document.getElementById("sex");
+    const inputText = document.getElementById("showWorkers");
+    //Values
+    const nameValue = inputName.value;
+    const hoursValue = parseInt(inputHours.value);
+    const sexValue = parseInt(inputSex.value);
+
+    if((nameValue !== "") && (!isNaN(hoursValue)) &&(sexValue !== 0)){
+        //Push
+        workers.push({
+            name: nameValue,
+            hours: hoursValue,
+            sex: sexValue
+        });
+    
+        //Write text
+        const basicText = writeText(workers);
+        const printText = basicText.reduce(obtainText,"");
+    
+        inputText.innerText = printText;
+    
+        //Final
+        inputForm.reset();
+    }else
+        alert("Complete the data");
+
+}
+
+function resetWorker(){
+    //Inputs
+    const inputForm = document.getElementById("workerForm");
+    const inputText = document.getElementById("showWorkers");
+    
+    //Restar array
+    restarArray(workers);
+    inputText.innerText = "";
+
+    //Final
+    inputForm.reset();
+
+}
+
+function calculateData(){
+    //Inputs
+    const inputPayHours = document.getElementById("payHour");
+    const inputMinHours = document.getElementById("minHours");
+    const inputBonus = document.getElementById("bonus");
+
+    const inputTotal = document.getElementById("total");
+    const inputAverange = document.getElementById("averange");
+    const inputNoQuota = document.getElementById("noQuota");
+
+    //Values
+    const valuePayHours = parseInt(inputPayHours.value);
+    const valueMinHours = parseInt(inputMinHours.value);
+    const valueBonus = parseInt(inputBonus.value);
+
+    if(!isNaN(valuePayHours) && !isNaN(valueMinHours)){
+        //Total
+        let total;
+        if(isNaN(valueBonus)){
+            total = totalToPay(workers,valueMinHours,valuePayHours);
+        }else
+            total = totalToPay(workers,valueMinHours,valuePayHours,valueBonus);
+
+        inputTotal.innerText = (`The total to pay is: ${total}`);
+        //Averange
+        const averangeHoursValue = averangeHours(workers);
+
+        inputAverange.innerText = (`Averange of hours worked: ${averangeHoursValue}`);
+        //Not Comply
+        const workerNotComply = hoursQuote(workers,valueMinHours);
+        const textNotComply = workerNotComply.map(
+            function (element){
+                const paragraph = `${element.name} worked ${element.hours} hours`;
+    
+                return paragraph
+            }
+        );
+        const printNotComply = textNotComply.reduce(obtainText,"");
+        
+        inputNoQuota.innerText = printNotComply;
+
+    }else
+        alert("Complete the data");
+
+}
 //Example
 workers.push({
     name: "a",
